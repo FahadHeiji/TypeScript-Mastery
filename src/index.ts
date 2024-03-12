f3043c30b0b02dc105297367c1f0b67e5e8f5c74
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-12 15:21:26 +0300

    init: setup Lesson Files

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
 /*
-  Type Annotations With Arrays
+  Type Annotations With Multidimensional Arrays
 */
-
-let all: string | number | boolean = "Osama";
-
-all = "A";
-all = 100;
-all = true;
-
-let myFriends: string[] = ["Osama", "Ahmed", "Sayed"];
-
-for (let i = 0; i < myFriends.length; i++) {
-  console.log(myFriends[i].repeat(3));
-}
