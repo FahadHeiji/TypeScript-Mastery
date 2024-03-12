ceeecac29497e5b663ef66bb217d7da09c648b31
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-12 16:22:35 +0300

    update: Lesson Files

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,9 @@
   - In JavaScript, Every Parameter Is Optional => "undefined" If You Didnt Use It
   - "?" Optional Parameter
 */
+
+function showData(name?: string, age?: number, country?: string) {
+    return `${name} - ${age} - ${country}`;
+  }
+  
+  console.log(showData("Osama", 40, "Egypt"));
