a8d8d3620b12e9ac5c7a8d044a688524064bed8c
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-12 16:51:43 +0300

    update: Lesson Files

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,13 @@
   - Anonymous Function
   - Arrow Function
 */
+
+const add = function (num1: number, num2: number): number {
+  return num1 + num2;
+};
+
+console.log(add(10, 20));
+
+const addWithArrow = (num1: number, num2: number): number => num1 + num2;
+
+console.log(addWithArrow(10, 20));
