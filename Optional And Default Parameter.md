6bdf22d7bfef14d0838d302d59ae6224b27a0258
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-12 16:17:00 +0300

    init: setup Lesson Files

---
+++ Optional And Default Parameter.md
@@ -0,0 +1,27 @@
+**Press `ctrl+shift+v` to preview.**
+
+---
+
+## Link To The Video :
+
+- [Optional And Default Parameter](https://www.youtube.com/watch?v=IS2VuO0IWso&list=PLDoPjvoNmBAy532K9M_fjiAmrJ0gkCyLJ&index=9).
+
+## Notes:
+
+- You Can Check For The Notes And Any other INFO When You Checkout:
+
+  ```git
+  git checkout L9-end
+  ```
+
+## OR Take Your Own Notes!
+
+Create a markdown file (e.g., notes.md) to capture your key takeaways while watching:
+
+**While watching:**
+
+- Stop the video when something important comes up.
+- Write down what you learned in simple words.
+- Ask any questions you have.
+
+---
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
 /*
-  Type Annotations With Functions
+  Function
+  - Optional and Default Parameters
 
-  - noImplicitAny
-
-  - noImplicitReturns
-  --- Will Check All Code Paths In A Function To Ensure They Return A Value
-
-  - noUnusedLocals
-  --- Report Errors On Unused Local Variables
-
-  - noUnusedParameters
-  --- Report Errors On Unused Parameters In Functions.
+  - In JavaScript, Every Parameter Is Optional => "undefined" If You Didnt Use It
+  - "?" Optional Parameter
 */
-
-let showMsg = true;
-
-function showDetails(name: string, age: number, salary: number) : string {
-  let test = 10;
-  if (showMsg) {
-    return `Hello ${name}, Age Is ${age}, Salary Is ${salary}, Test Variable ${test}`;
-  }
-  return `No Data To Show`;
-}
-
-console.log(showDetails("Osama", 40, 5000));
