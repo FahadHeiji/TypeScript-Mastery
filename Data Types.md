32d490f012d3cb35040bbffcd65401f2ce5fcd18
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-17 17:10:47 +0300

    init: setup Assignments Files

---
+++ Data Types.md
@@ -0,0 +1,17 @@
+**Press `ctrl+shift+v` to preview.**
+
+---
+
+## Link To The Assignments :
+
+- [Data Types](https://elzero.org/typescript-assignments-lessons-from-12-to-21/).
+
+## Solutions:
+
+- You Can Check the solutions When You Checkout:
+
+  ```git
+  git checkout W2-end
+  ```
+
+---
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,63 @@
 /*
-  Type Annotations With Object
+    Typescript Assignments Lessons From 1-to-11
+
+    Before You Start Search For:
+
+        - Typescript Data Types
+
+        -Typescript Unknown
+
+        -Typescript Type Aliases
+
+        -Typescript Type Aliases Extend
+
+        -Typescript Type Examples
+
+        -Typescript Union
+      
+        */
+
+/*
+        اسْتَعِنْ باللَّهِ وَلَا تَعْجِزْ
 */
 
-let myObject: {
-  readonly username: string,
-  id: number,
-  hire?: boolean,
-  skills: {
-    one: string,
-    two: string
-  }
-} = {
-  username: "Elzero",
-  id: 100,
-  hire: true,
-  skills: {
-    one: "HTML",
-    two: "CSS"
-  }
-};
+// Q-01:
+console.groupCollapsed();
+// Edit tsconfig.json File
 
-// myObject.username = "Osama";
-myObject.id = 101;
-myObject.hire = false;
+console.groupEnd();
 
-console.log(myObject.username);
-console.log(myObject.id);
-console.log(myObject.hire);
-console.log(myObject.skills.one);
+// Q-02:
+console.groupCollapsed();
+
+console.groupEnd();
+
+// Q-03:
+console.groupCollapsed();
+
+console.groupEnd();
+
+// Q-04:
+console.groupCollapsed();
+
+console.groupEnd();
+
+// Q-05:
+console.groupCollapsed();
+
+console.groupEnd();
+
+// Q-06:
+console.groupCollapsed();
+
+console.groupEnd();
+
+// Q-07:
+console.groupCollapsed();
+
+console.groupEnd();
+
+// Q-08:
+console.groupCollapsed();
+
+console.groupEnd();
