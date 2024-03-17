0a2eaa9f6ec21ec8156745c529e94bd41592b976
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-17 16:35:25 +0300

    update: Lesson Files

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,10 @@
   --- Sometime Compiler Doesnt Know The Information We Do
   --- TypeScript Is Not Performing Any Check To Make Sure Type Assertion Is Valid
 */
+
+// let myImg = document.getElementById("my-img") as HTMLImageElement;
+let myImg = <HTMLImageElement> document.getElementById("my-img");
+console.log(myImg.src);
+
+let data: any = 1000;
+console.log((data as string).repeat(3));
