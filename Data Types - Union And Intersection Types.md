b11e95aaed80f810cdee95ccadfab85eb30d0e44
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-17 16:37:57 +0300

    init: setup Lesson Files

---
+++ Data Types - Union And Intersection Types.md
@@ -0,0 +1,27 @@
+**Press `ctrl+shift+v` to preview.**
+
+---
+
+## Link To The Video :
+
+- [Data Types - Union And Intersection Types](https://www.youtube.com/watch?v=Uf-ODplNJ7A&list=PLDoPjvoNmBAy532K9M_fjiAmrJ0gkCyLJ&index=20).
+
+## Notes:
+
+- You Can Check For The Notes And Any other INFO When You Checkout:
+
+  ```git
+  git checkout L20-end
+  ```
+
+## OR Take Your Own Notes!
+
+Create a markdown file (e.g., notes.md) to capture your key takeaways while watching:
+
+**While watching:**
+
+- Stop the video when something important comes up.
+- Write down what you learned in simple words.
+- Ask any questions you have.
+
+---
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 /*
   Data Types
-  - Type Assertions
-  --- Sometime Compiler Doesnt Know The Information We Do
-  --- TypeScript Is Not Performing Any Check To Make Sure Type Assertion Is Valid
+  - Union And Intersection Types
+  --- Union Type
+  ------ The | Symbol Is Used To Create The Union => "Or"
+
+  --- Intersection Type
+  ------ Is A Type That Combines Several Types Into One
+  ------ The & Symbol Is Used To Create An Intersection => "And"
+
+  --- If A Union Is An OR, Then An Intersection Is An AND.
 */
-
-// let myImg = document.getElementById("my-img") as HTMLImageElement;
-let myImg = <HTMLImageElement> document.getElementById("my-img");
-console.log(myImg.src);
-
-let data: any = 1000;
-console.log((data as string).repeat(3));
