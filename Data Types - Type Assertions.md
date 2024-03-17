2819288470211f0e87acda62b4782ece3a0bb520
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-17 15:46:23 +0300

    init: setup Lesson Files

---
+++ Data Types - Type Assertions.md
@@ -0,0 +1,27 @@
+**Press `ctrl+shift+v` to preview.**
+
+---
+
+## Link To The Video :
+
+- [Data Types - Type Assertions](https://www.youtube.com/watch?v=tPSHBw_2huc&list=PLDoPjvoNmBAy532K9M_fjiAmrJ0gkCyLJ&index=19).
+
+## Notes:
+
+- You Can Check For The Notes And Any other INFO When You Checkout:
+
+  ```git
+  git checkout L19-end
+  ```
+
+## OR Take Your Own Notes!
+
+Create a markdown file (e.g., notes.md) to capture your key takeaways while watching:
+
+**While watching:**
+
+- Stop the video when something important comes up.
+- Write down what you learned in simple words.
+- Ask any questions you have.
+
+---
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,6 @@
 /*
   Data Types
-  - Enums => Enumerations
-  --- Enum Can Refer To Other Enum
-  --- Enum Can Refer To Same Enum
-  --- Enum Can Have Calculations
-  --- Enum Can Have Functions
+  - Type Assertions
+  --- Sometime Compiler Doesnt Know The Information We Do
+  --- TypeScript Is Not Performing Any Check To Make Sure Type Assertion Is Valid
 */
-
-function getHardSeconds() : number {
-  return 3;
-}
-
-enum Kids {
-  Five = 25,
-  Seven = 20,
-  Ten = 15
-}
-
-enum Level {
-  Kid = Kids.Ten,
-  Easy = 9,
-  Medium = Easy - 3,
-  Hard = getHardSeconds()
-}
-
-let lvl: string = "Easy";
-
-if (lvl === "Easy") {
-  console.log(`The Level Is ${lvl} And Number Of Seconds Is ${Level.Hard}`);
-}
