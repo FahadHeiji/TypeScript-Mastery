200ba4c8d8a7824af8e644bb242ab97a997208f1
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-18 15:27:52 +0300

    init: setup Lesson Files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 /*
-  Type Annotations With Class
+  Class
+  - Data Access Modifiers & Parameters Properties
+  --- Public
+  ------ All Members Of A Class In TypeScript Are Public
+  ------ All Public Members Can Be Accessed Anywhere Without Any Restrictions
+  --- Private
+  ------ Members Are Visible Only To That Class And Are Not Accessible Outside The Class
+  --- Protected
+  ------ Same Like Private But Can Be Accessed Using The Deriving Class
+
+  - TypeScript Is A Layer On Top Of JavaScript
+  - It Should Remove All Annotations And Although Access Modifiers "Private For Example"
 */
 
 class User {
