f1e2b4e9a1c1e8dd976999e6bd33d35549d6333a
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-18 16:20:58 +0300

    init: setup Lesson Files

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,5 @@
 /*
   Class
-  - Get And Set Accessors
+  - Static Members
+  --- Don't Use "name, length, call"
 */
-
-class User {
-  public get username(): string {
-    return this._username;
-  }
-  public set username(value: string) {
-    this._username = value;
-  }
-  msg: () => string;
-  constructor(private _username: string, public salary: number, public readonly address: string) {
-    this.msg = function () {
-      return `Hello ${this._username} Your Salary Is ${this.salary}`;
-    }
-  }
-  sayMsg() {
-    return `Hello ${this._username} Your Salary Is ${this.salary}`;
-  }
-  // get username() : string {
-  //   return this._username;
-  // }
-  // set username(value: string) {
-  //   this._username = value;
-  // }
-}
-
-let userOne = new User("Elzero", 6000, "Cairo");
-
-console.log(userOne.username);
-userOne.username = "Ahmed";
-console.log(userOne.username);
-console.log(userOne.salary);
-console.log(userOne.msg());
-console.log(userOne.sayMsg());
