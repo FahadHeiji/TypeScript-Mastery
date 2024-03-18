eee18b48f487235d563405477c67ac867d57ac52
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-18 14:29:29 +0300

    init: setup Lesson Files

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,4 @@
 /*
   Interface
-  - Interface Declaration
-  --- Serve Like Types
-  --- The Interface Describes The Shape Of An Object
-  --- It Defines The Syntax To Follow
-
-  --- Use With Object
-  --- Use With Function
-  --- Use Read Only And Optional Operator
+  - Interface Method And Parameters
 */
-
-interface User {
-  id?: number,
-  readonly username: string,
-  country: string
-}
-
-let user: User = {
-  id: 100,
-  username: "Elzero",
-  country: "Egypt"
-}
-
-user.country = "Syria";
-
-console.log(user);
-
-function getData(data: User) {
-  console.log(`Id Is ${data.id}`);
-  console.log(`Username Is ${data.username}`);
-  console.log(`Country Is ${data.country}`);
-}
-
-getData({ id: 200, username: "Osama", country: "KSA" });
