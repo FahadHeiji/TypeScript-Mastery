e4312a5a13748a98b889f7df8c597f2798dc2a74
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-18 14:47:52 +0300

    init: setup Lesson Files

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,4 @@
 /*
   Interface
-  - Interface Method And Parameters
+  - ReOpen The Interface And Use Cases
 */
-
-interface User {
-  id: number;
-  username: string;
-  country: string;
-  sayHello() : string;
-  sayWelcome: () => string;
-  getDouble(num: number) : number;
-}
-
-let user: User = {
-  id: 100,
-  username: "Elzero",
-  country: "Egypt",
-  sayHello() {
-    return `Hello ${this.username}`;
-  },
-  sayWelcome: () => {
-    return `Welcome ${user.username}`;
-  },
-  getDouble(n) {
-    return n * 2;
-  }
-}
-
-console.log(user.id);
-console.log(user.sayHello());
-console.log(user.sayWelcome());
-console.log(user.getDouble(100));
