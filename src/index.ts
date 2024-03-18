8b64244545b81fecca80c96c16c9eb3213445a13
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-18 15:20:13 +0300

    init: setup Lesson Files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
 /*
   Type Annotations With Class
 */
+
+class User {
+  u: string;
+  s: number;
+  msg: () => string;
+  constructor(username: string, salary: number) {
+    this.u = username;
+    this.s = salary;
+    this.msg = function () {
+      return `Hello ${this.u} Your Salary Is ${this.s}`;
+    }
+  }
+  sayMsg() {
+    return `Hello ${this.u} Your Salary Is ${this.s}`;
+  }
+}
+
+let userOne = new User("Elzero", 6000);
+
+console.log(userOne.u);
+console.log(userOne.s);
+console.log(userOne.msg());
+console.log(userOne.sayMsg());
