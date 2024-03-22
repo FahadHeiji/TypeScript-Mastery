505e039da42b64dbe227a8c96e2084d7c03a555a
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-22 14:34:28 +0300

    init: setup Lesson Files

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,13 @@
 /*
   Class
-  - Abstract Classes And Members
-  --- We Cannot Create An Instance Of An Abstract Class
+  - Polymorphism & Method Override
+
+  - Polymorphism
+  --- Classes Have The Same Methods But Different Implementations
+
+  - Method Override
+  --- Allowing Child Class To Provide Implementation Of A Method In Parent Class
+  --- A Method In Child Class Must Have Same Name As Parent Class
+
+  --- noImplicitOverride
 */
-
-abstract class Food {
-  constructor(public title: string) {}
-  abstract getCookingTime() : void;
-}
-
-class Pizza extends Food {
-  constructor(title: string, public price: number) {
-    super(title);
-  }
-  getCookingTime() : void {
-    console.log(`Cooking Time For Pizza Is 1 Hour`);
-  }
-}
-
-class Burger extends Food {
-  constructor(title: string, public price: number) {
-    super(title);
-  }
-  getCookingTime() : void {
-    console.log(`Cooking Time For Burger Is Half Hour`);
-  }
-}
-
-let pizzaOne = new Pizza("Awesome Pizza", 100);
-
-console.log(pizzaOne.title);
-console.log(pizzaOne.price);
-pizzaOne.getCookingTime();
