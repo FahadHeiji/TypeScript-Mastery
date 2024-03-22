b9be97f1fad589aca55ec1a5231d47f7088e83a3
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-22 15:06:59 +0300

    init: setup Lesson Files

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,7 @@
 /*
-  Generics
-  - Classes And Interfaces
+  - How To Continue
+  - Practice
+  - Other Topics Not In The Course
+  - JSDocs
+  - TsConfig
 */
-
-interface Book {
-  itemType: string;
-  title: string;
-  isbn: number;
-}
-
-interface Game {
-  itemType: string;
-  title: string;
-  style: string;
-  price: number;
-}
-
-class Collection<T> {
-  public data: T[] = [];
-  add(item: T) : void {
-    this.data.push(item);
-  }
-}
-
-let itemOne = new Collection<Book>();
-itemOne.add({ itemType: "Book", title: "Atomic Habits", isbn: 150510 });
-itemOne.add({ itemType: "Book", title: "Follow Your Heart", isbn: 650650 });
-console.log(itemOne);
-
-let itemTwo = new Collection<Game>();
-itemTwo.add({ itemType: "Game", title: "Uncharted", style: "Action", price: 150 });
-console.log(itemTwo);
