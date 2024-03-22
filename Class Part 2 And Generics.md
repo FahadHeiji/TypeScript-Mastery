08e0ad2bf84d1dfb06ef00ba27794a54a3f9d621
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-22 15:17:09 +0300

    init: setup Assignments Files

---
+++ Class Part 2 And Generics.md
@@ -0,0 +1,17 @@
+**Press `ctrl+shift+v` to preview.**
+
+---
+
+## Link To The Assignments :
+
+- [Class Part 2 And Generics](https://elzero.org/typescript-assignments-lessons-from-32-to-38/).
+
+## Solutions:
+
+- You Can Check the solutions When You Checkout:
+
+  ```git
+  git checkout W4-end
+  ```
+
+---
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
 /*
-  - How To Continue
-  - Practice
-  - Other Topics Not In The Course
-  - JSDocs
-  - TsConfig
+    Typescript Assignments Lessons From 32-to-38
+
+    Before You Start Search For:
+
+      - Typescript Generics Interface
+      - Typescript Generics Types
+      - Typescript Optional Generics
+      - Typescript Generics Use Cases
+      - Typescript Generics And Classes
+      - Typescript Method Override
+      
+        */
+
+/*
+        اسْتَعِنْ باللَّهِ وَلَا تَعْجِزْ
 */
+
+// Q-01:
+console.groupCollapsed();
+
+
+console.groupEnd();
+
+// Q-02:
+console.groupCollapsed();
+
+console.groupEnd();
+
+// Q-03:
+console.groupCollapsed();
+
+console.groupEnd();
