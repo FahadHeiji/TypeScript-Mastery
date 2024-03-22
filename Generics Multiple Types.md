1b081ca62bc77473cc9ad2f61b12071afffc0572
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-22 14:46:49 +0300

    init: setup Lesson Files

---
+++ Generics Multiple Types.md
@@ -0,0 +1,27 @@
+**Press `ctrl+shift+v` to preview.**
+
+---
+
+## Link To The Video :
+
+- [Generics Multiple Types](https://www.youtube.com/watch?v=BA40TsV6LxQ&list=PLDoPjvoNmBAy532K9M_fjiAmrJ0gkCyLJ&index=35).
+
+## Notes:
+
+- You Can Check For The Notes And Any other INFO When You Checkout:
+
+  ```git
+  git checkout L35-end
+  ```
+
+## OR Take Your Own Notes!
+
+Create a markdown file (e.g., notes.md) to capture your key takeaways while watching:
+
+**While watching:**
+
+- Stop the video when something important comes up.
+- Write down what you learned in simple words.
+- Ask any questions you have.
+
+---
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,14 @@
 /*
   Generics
-  - Help Write A Reusable Code
-  - Allow To Pass Type As A Parameter To Another Type
-  - You Will Be Able To Deal With Multiple Types Without Using ": Any Type"
-  - We Can Create:
-  --- Generic Classes
-  --- Generic Functions
-  --- Generic Methods
-  --- Generic Interfaces
+  - Arrow Function
+  - Multiple Types
+  - Discussion
 */
 
-function returnNumber(val: number) : number {
-  return val;
-}
-function returnString(val: string) : string {
-  return val;
-}
-function returnBoolean(val: boolean) : boolean {
-  return val;
-}
-
-console.log(returnNumber(100));
-console.log(returnString("Elzero"));
-console.log(returnBoolean(true));
-
-function returnType<T>(val: T) : T {
+function returnType<T>(val: T): T {
   return val;
 }
 
 console.log(returnType<number>(100));
 console.log(returnType<string>("Elzero"));
-console.log(returnType<boolean>(true));
-console.log(returnType<number[]>([1, 2, 3, 4]));
+
