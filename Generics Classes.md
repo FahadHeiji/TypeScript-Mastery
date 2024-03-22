ff6a29b08b7de1109d371017eaf625fbee4521cd
FahadHeiji <fahadmohamedar@gmail.com>
2024-03-22 14:50:41 +0300

    init: setup Lesson Files

---
+++ Generics Classes.md
@@ -0,0 +1,27 @@
+**Press `ctrl+shift+v` to preview.**
+
+---
+
+## Link To The Video :
+
+- [Generics Classes](https://www.youtube.com/watch?v=Mj6TJAoisxw&list=PLDoPjvoNmBAy532K9M_fjiAmrJ0gkCyLJ&index=36).
+
+## Notes:
+
+- You Can Check For The Notes And Any other INFO When You Checkout:
+
+  ```git
+  git checkout L36-end
+  ```
+
+## OR Take Your Own Notes!
+
+Create a markdown file (e.g., notes.md) to capture your key takeaways while watching:
+
+**While watching:**
+
+- Stop the video when something important comes up.
+- Write down what you learned in simple words.
+- Ask any questions you have.
+
+---
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,4 @@
 /*
   Generics
-  - Arrow Function
-  - Multiple Types
-  - Discussion
+  - Classes
 */
-
-function returnType<T>(val: T): T {
-  return val;
-}
-
-console.log(returnType<number>(100));
-console.log(returnType<string>("Elzero"));
-
-const returnTypeArrowSyntax = <T>(val: T): T => val;
-
-console.log(returnTypeArrowSyntax<number>(100));
-console.log(returnTypeArrowSyntax<string>("Elzero"));
-
-function testType<T>(val: T): string {
-  return `The Value Is ${val} And Type Is ${typeof val}`;
-}
-
-console.log(testType<number>(100));
-console.log(testType<string>("Elzero"));
-
-function multipleTypes<T, S>(valueOne: T, valueTwo: S): string {
-  return `The First Value Is ${valueOne} And Second Value ${valueTwo}`;
-}
-
-console.log(multipleTypes<string, number>("Osama", 100));
-console.log(multipleTypes<string, boolean>("Elzero", true));
